fd5b3f44853b0784098f8172ebe5e756ce718a36
yutthapichai <o_yutthapichai.c@kubix.co>
2023-01-03 13:40:18 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,47 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault, ApolloServerPluginInlineTraceDisabled } from 'apollo-server-core';
+import { RemoteGraphQLDataSource } from '@apollo/gateway';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.config.env']
+    }),
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        // ... Apollo server options
+        introspection: true,
+        playground: false,
+        plugins: [
+          ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+          ApolloServerPluginInlineTraceDisabled()
+        ],
+        cors: {
+          origin: ["https://sandbox.embed.apollographql.com", "http://localhost:3000"]
+        },
+      },
+      gateway: {
+        serviceList: [
+          { name: 'auth', url: 'http:/0.0.0.0:4011/graphql' },
+          { name: 'product', url: 'http:/0.0.0.0:4012/graphql' },
+        ],
+        buildService({ url }) {
+          return new (class extends RemoteGraphQLDataSource {
+            willSendRequest({ request, context }) {
+              context.req?.headers ? request.http.headers.set('authorization', context.req?.headers?.authorization) : null;
+            }
+          })({ url });
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = Number(process.env.PORT)
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  await app.listen(port , '0.0.0.0').then(() => {
+    console.log(`http://localhost:${port}`)
+  });
 }
 bootstrap();
